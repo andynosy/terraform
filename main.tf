64e5149457afc5a3f8cb2e75b87ed5f2ef82b0fe
Andrew <andynosy@yahoo.com>
2020-01-06 02:03:39 -0500

    correction

--- main.tf
+++ main.tf
@@ -9,6 +9,8 @@ resource "aws_iam_instance_profile" "s3_access_profile" {
   name = "s3_access"
   role = "${aws_iam_role.s3_access_role.name}"
 
+}
+
 resource "aws_iam_role_policy" "s3_acess_policy" {
   name = "s3_access_policy"
   role = "${aws_iam_role.s3_access_role.id}"
@@ -23,7 +25,6 @@ resource "aws_iam_role_policy" "s3_acess_policy" {
        "Resource": "*"
 }
    ]
-
 
 }
 
