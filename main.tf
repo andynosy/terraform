23b594555605e5a4488b849d2439f23fcb20267f
Andrew <andynosy@yahoo.com>
2020-01-09 00:51:30 -0500

    added security group

--- main.tf
+++ main.tf
@@ -209,3 +209,103 @@ resource "aws_route_table_association" "wp_private2_assoc" {
   route_table_id = "${aws_default_route_table.wp_private_rt.id}"
 
 }
+
+# security groups
+
+resource "aws_security_group" "wp_dev_sg" {
+  name        = "wp_dev_sg"
+  description = "used for access to dev instance"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  #ssh
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+ #HTTP
+  ingress {
+    from_port   = "80"
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+}
+  egress {
+    to_port     = 0
+    from_port   = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+}
+
+resource "aws_security_group" "wp_public_sg" {
+  name        = "wp_public_sg"
+  description = "used for the elastic load balancer fpr public access"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+ 
+# http
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  
+       }
+
+}
+
+
+# private security group
+
+resource "aws_security_group" "wp_private_sg" {
+  name        = "wp_private_sg"
+  description = " used by private subnet host "
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["${var.vpc_cidr}"]
+}
+   egress {
+     from_port   = 0
+     to_port     = 0
+     protocol    = "-1"
+     cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+}
+
+
+# RDS security group 
+resource "aws_security_group" "wp_rds_sg" {
+  name        = "wp_rds_sg"
+  description = "for rdb instances access"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+ ingress {
+   from_port   = 3306
+   to_port     = 3306
+   protocol    = "tcp"
+   cidr_blocks = ["${var.localip}"]
+   security_groups = ["${aws_security_group.wp_dev_sg.id}",
+                    "${aws_security_group.wp_public_sg.id}",
+                    "${aws_security_group.wp_private_sg.id}",
+                    ]
+
+}
+
+}
+
